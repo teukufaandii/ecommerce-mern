1531e8b2f0462acd46eaff6e1f451310f7f85496
Teuku Fandi Ahmad <tfandiahmad32@gmail.com>
2025-01-21 21:03:30 +0700

    adding analytics routes for data sum

--- server/index.js
+++ server/index.js
@@ -10,6 +10,7 @@ import productRoutes from "./routes/product.route.js";
 import cartRoutes from "./routes/cart.route.js";
 import couponRoutes from "./routes/coupon.route.js";
 import paymentRoutes from "./routes/payment.route.js";
+import analyticsRoutes from "./routes/analytic.route.js";
 
 dotenv.config();
 
@@ -20,10 +21,11 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
-app.use("/api/products", productRoutes)
-app.use("/api/carts", cartRoutes)
-app.use("/api/coupons", couponRoutes)
-app.use("/api/orders", paymentRoutes)
+app.use("/api/products", productRoutes);
+app.use("/api/carts", cartRoutes);
+app.use("/api/coupons", couponRoutes);
+app.use("/api/orders", paymentRoutes);
+app.use("/api/analytics", analyticsRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
